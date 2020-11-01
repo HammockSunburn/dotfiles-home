f0a6116f3b6e86752bf4e4a211e2b744bc48aade
Hammock Sunburn <hammocksunburn@gmail.com>
2020-11-01 10:23:34 -0600

    Added fish function to compare sha256 written on usb vs file

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -9,3 +9,15 @@ if test -e /opt/boost
    set -x BOOST_ROOT /opt/boost 
 end
 
+function checkiso -d "Compare an ISO file to what's been written to a USB drive"
+    sudo true
+    echo "reading USB checksum from" $argv[2]
+    echo "reading file checksum from" $argv[1]
+
+    set iso_size (stat -c '%s' $argv[1])
+    set usb_sha (sudo dd bs=4096 if=$argv[2] | pv -p -b -s $iso_size -S | sha256sum)
+    set file_sha (pv -p -b -s $iso_size $argv[1] | sha256sum)
+
+    echo "USB checksum: " $usb_sha
+    echo "File checksum:" $file_sha
+end
