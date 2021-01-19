91a0f4aa695c720f30d4a5faf8f97a29cb25045d
Hammock Sunburn <hammocksunburn@gmail.com>
2021-01-19 09:51:35 -0600

    Update prescient-filter-method

--- emacs/init.el
+++ emacs/init.el
@@ -468,4 +468,5 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(avy-background t))
+ '(avy-background t)
+ '(prescient-filter-method '(literal regexp initialism fuzzy)))
