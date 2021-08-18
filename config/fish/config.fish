dd54f1105a7caba49574ba1c1899db2c18bb3b2a
Hammock Sunburn <hammocksunburn@gmail.com>
2021-08-18 10:31:24 -0500

    Various cleanup, remove starship from fish.

--- config/fish/config.fish
+++ config/fish/config.fish
@@ -1,17 +1,11 @@
-starship init fish | source
 zoxide init fish | source
 
 alias ls="exa --time-style=long-iso"
 alias ping="prettyping --nolegend"
 alias icat="kitty +kitten icat --align=left"
-alias conan="$HOME/conan/bin/conan"
 
 if test -d /opt/boost
-    set -x BOOST_ROOT /opt/boost 
-end
-
-if test -d "$HOME/software/emacs"
-    set -gx PATH "$HOME/software/emacs/bin" $PATH
+    set -gx BOOST_ROOT /opt/boost 
 end
 
 if test -d "$HOME/bin"
@@ -20,15 +14,15 @@ end
 
 function checkiso -d "Compare an ISO file to what's been written to a USB drive"
     sudo true
-    echo "reading USB checksum from" $argv[2]
     echo "reading file checksum from" $argv[1]
+    echo "reading USB checksum from" $argv[2]
 
     set iso_size (stat -c '%s' $argv[1])
     set usb_sha (sudo dd bs=4096 if=$argv[2] | pv -p -b -s $iso_size -S | sha256sum)
     set file_sha (pv -p -b -s $iso_size $argv[1] | sha256sum)
 
-    echo "USB checksum: " $usb_sha
     echo "File checksum:" $file_sha
+    echo "USB checksum: " $usb_sha
 end
 
 # ghcup-env
